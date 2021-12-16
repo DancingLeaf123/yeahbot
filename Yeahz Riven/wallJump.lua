8dc8c431268d7d16c7565042e623133754ce732f
DancingLeaf123 <851280706@qq.com>
2021-12-17 01:14:35 +0800

    walljump

--- Yeahz Riven/wallJump.lua
+++ Yeahz Riven/wallJump.lua
@@ -30,12 +30,13 @@ local WallJump = function ()
     local qSlot = player:spellSlot(0)
     local q = pred.q.get_spell_state()
     local e = pred.e.get_spell_state()
-    if q and qSlot.stacks ~= 2 then
+    if q and qSlot.stacks < 2 then
       player:castSpell('pos', 0, mousePos)
     end
 
     -- We need to define a new move position since jumping over walls
     -- requires you to be close to the specified wall. Therefore we set the move
+    
     local wallCheck = GetFirstWallPoint(player.pos, mousePos, 25)
     
     -- Be more precise
@@ -43,11 +44,10 @@ local WallJump = function ()
       wallCheck = GetFirstWallPoint(wallCheck:to3D(), mousePos, 5)
     end
     
-    local movePosition = wallCheck ~= nil and wallCheck:to3D() or mousePos
-    player:move(movePosition)
+    local movePosition = wallCheck ~= nil and q and wallCheck:to3D() or mousePos
     -- var tempGrid = NavMesh.WorldToGrid(movePosition.X, movevPosition.Y);
     -- Program.FleePosition = NavMesh.GridToWorld((short)tempGrid.X, (short)tempGrid.Y);
-    
+    local IsJumpPossible = false
     if wallCheck ~= nil then
       local wallPosition = movePosition;
       local direction = (mousePos:to2D() - wallPosition:to2D()):norm()
@@ -55,6 +55,7 @@ local WallJump = function ()
       local step = maxAngle / 20
       local currentAngle = 0;
       local currentStep = 0;
+      local jumpTriggered = false
       while(true)
       do
         if (currentStep > maxAngle and currentAngle < 0) then
@@ -69,18 +70,17 @@ local WallJump = function ()
         local checkPoint = vec3(0, 0, 0)
         if (currentStep == 0) then
             currentStep = step;
-            checkPoint = wallPosition + 500 * direction:to3D();
+            checkPoint = wallPosition + 435 * direction:to3D();
         else
-            checkPoint = wallPosition + 467.5 * direction:rotate(currentAngle):to3D();
+            checkPoint = wallPosition + 435 * direction:rotate(currentAngle):to3D();
         end
-            graphics.draw_line(checkPoint, player.pos, 2, 0xFFFFFFFF)
+            graphics.draw_line(checkPoint, wallPosition, 2, 0xFFFFFFFF)
         if not(navmesh.isWall(checkPoint) or navmesh.isStructure(checkPoint)) then
           wallCheck = GetFirstWallPoint(checkPoint, wallPosition);
           if (wallCheck ~= nil) then
             local firstWallPoint = GetFirstWallPoint(wallCheck:to3D(), wallPosition);
             if (firstWallPoint ~= nil) then
               local wallPositionOpposite = firstWallPoint:to3D();
-              graphics.draw_line(wallPositionOpposite, player.pos, 2, 0xFFFF0000)
               local allpath,n = player.path:calcPos(wallPositionOpposite)
               local temptable = {}
               for i = 0, n - 1 do
@@ -88,31 +88,41 @@ local WallJump = function ()
                 table.insert(temptable, allpath[i])
               end
               local allpathlen = GetPathLength(temptable)
-              print("allpathlen",allpathlen)
-              if (allpathlen - player.pos:dist(wallPositionOpposite) > 200) then
-                print(">200")
-                print("player.boundingRadius / 2",player.boundingRadius / 2)
-                print("player.pos:dist(wallPositionOpposite)",player.pos:dist(wallPositionOpposite))
-                print("player.pos:distSqr(wallPositionOpposite)",player.pos:distSqr(wallPositionOpposite))
-                if (player.pos:distSqr(wallPositionOpposite) < (510 - player.boundingRadius / 2)^2 and qSlot.stacks == 2) then
-                  print("cast E ----------------------")
-                  print("cast E ----------------------",player.pos:distSqr(wallPositionOpposite))
+              -- print("allpathlen",allpathlen)
+              if (allpathlen - player.pos:dist(wallPositionOpposite) > 280) then
+                player:move(movePosition)
+                graphics.draw_line(wallPositionOpposite, wallPosition, 2, 0xFF008000)
+                if (player.pos:distSqr(wallPositionOpposite) < (480 - player.boundingRadius / 2)^2 and qSlot.stacks >= 2) then
+                  graphics.draw_line(wallPositionOpposite, wallPosition, 2, 0xFFFF0000)
+                  print("cast E ----------------------",(480 - player.boundingRadius / 2))
+                  print("cast E ----------------------",player.pos:dist(wallPositionOpposite))
                   print("cast E ----------------------",(300 - player.boundingRadius / 2)^2)
                   if (e) then
                     player:castSpell('pos', 2, wallPositionOpposite)
                   elseif (q) then
                     player:castSpell('pos', 0, wallPositionOpposite)
                   end
+                  jumpTriggered = true
+                  print("jumpTriggered",jumpTriggered)
                   break;
                 end
+                IsJumpPossible = true
+              end
+              if not IsJumpPossible then
+                player:move(mousePos)
               end
             end
           end
         end
       end
+    else
+      player:move(mousePos)
     end
   end
 end
+-- if (not jumpTriggered) then
+--   player:move(mousePos)
+-- end
 
 orb.combat.register_f_pre_tick(function()
   WallJump()
