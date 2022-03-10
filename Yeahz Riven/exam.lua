6ec6ed96511a51c9a3e8a6afd89c31443eb993be
DancingLeaf123 <851280706@qq.com>
2022-03-10 21:48:10 +0800

    222

---
+++ Yeahz Riven/exam.lua
@@ -0,0 +1,19 @@
+-- Q2
+
+local update = function(dt)
+  local STATE0 = 0
+  local STATE1 = 1
+  local res = STATE0 or STATE1
+  if dt > 5 then
+    res = STATE1
+    for i=0,10000, increment
+    do
+      statement(s)
+    end
+  end
+
+  return res
+end
+
+-- Q3
+
